683cb0f0a91c4356fe6f0adf548059a1d9803b07
Idk07-dev <oscarantoniofernandezbaez@gmail.com>
2026-03-14 20:24:50 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -4,5 +4,8 @@ const addonInterface = require("../addon");
 const router = getRouter(addonInterface);
 
 module.exports = (req, res) => {
+    // Esto permite que Stremio se conecte desde cualquier dispositivo
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', '*');
     router(req, res);
 };
